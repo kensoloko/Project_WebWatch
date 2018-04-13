1812a9af35c0c0fe5145f4ec6feab92325f23105
kensoloko <kensoloko@gmail.com>
2018-04-13 12:50:31 +0700

    admin_layout_page

--- Gemfile
+++ Gemfile
@@ -39,3 +39,4 @@ group :test do
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "font-awesome-rails", "~> 4.7", ">= 4.7.0.4"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get "base/home"
+    get "base/products"
+    get "base/categories"
+    get "base/users"
+    get "base/brands"
+    get "base/orders"
+    get "base/comments"
+    get "base/ratings"
+    get "session/login"
+  end
 end
