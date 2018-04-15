60ebe0be980af7a118a1adc7c0a8e0ad61605b5a
hungkieu <hungkieu.h12@gmail.com>
2018-04-15 10:45:06 +0700

    cart done

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,3 +1,22 @@
 class CheckoutController < ApplicationController
-  def show; end
+  def index; end
+
+  def create
+    render html: bill_details
+  end
+
+  private
+
+  def bill_details
+    arr = []
+    params[:itemCount].to_i.times do |x|
+      x += 1
+      product_id = params["item_options_#{x}"][/[0-9]/].to_i
+      obj = {product_id: product_id, user_id: current_user.id,
+        quantity: params["item_quantity_#{x}"]}
+      arr.push obj
+    end
+    return arr
+  end
+
 end
